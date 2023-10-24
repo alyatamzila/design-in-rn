184be0504caa4d190334307ee6f428d3c20b0af2
alyatamzila <alya.selvia.21@student.is.ittelkom-sby.ac.id>
2023-10-24 11:51:27 +0700

    design in react native

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  LotsOfStyles,
+  FixedDimensionsBasics,
+  FlexDimensionsBasics,
+  PercentageDimensionsBasics,
+  FlexBasic,
+  FlexDirectionBasics,
+  JustifyContentBasics,
+  AlignItemsLayout,
+  WidthHeightBasics,
+  PositionLayout,
+  DisplayAnImageWithStyle,
+  NativeBaseBasic,
+  GlueStackUIBasic,
+} from "./screens";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+// Array of Object Declaration
+const pageArr = [
+  { name: "Lots Of Styles", comp: <LotsOfStyles /> },
+  { name: "Fixed Dimensions Basics", comp: <FixedDimensionsBasics /> },
+  { name: "Flex Dimensions Basics", comp: <FlexDimensionsBasics /> },
+  {
+    name: "Percentage Dimensions Basics",
+    comp: <PercentageDimensionsBasics />,
   },
-});
+  { name: "Flex Basic", comp: <FlexBasic /> },
+  { name: "Flex Direction Basics", comp: <FlexDirectionBasics /> },
+  { name: "Justify Content Basics", comp: <JustifyContentBasics /> },
+  { name: "Align Items Layout", comp: <AlignItemsLayout /> },
+  { name: "Width Height Basics", comp: <WidthHeightBasics /> },
+  { name: "Position Layout", comp: <PositionLayout /> },
+  { name: "Display An Image With Style", comp: <DisplayAnImageWithStyle /> },
+  { name: "Native Base Basic", comp: <NativeBaseBasic /> },
+  { name: "Gluestack UI Basic", comp: <GlueStackUIBasic /> },
+];
